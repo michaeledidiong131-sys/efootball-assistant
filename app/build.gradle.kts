ffb71ed6c7feacd237be85d378eaa2b20cb2a326
Edidiong Godwin Michael <michaeledidiong131@gmail.com>
2026-05-28 02:08:46 +0000

    Commit to "main"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
 }
